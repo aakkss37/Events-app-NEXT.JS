8dffd7e3416a08cebe827eb0e55b1eb2fb4cd784
amar kumar <amarkumar.sharma.124@gmail.com>
2024-01-16 02:03:59 +0530

    added home page content

---
+++ components/Events/EventCard.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import style from './eventCard.module.css'
+
+const EventCard = ({ event }) => {
+    const formattedDate = new Date(event.date).toLocaleDateString('en-US', { day: '2-digit', month: "short", year: "numeric" })
+    const formattedAddress = event.location.replace(', ', '\n')
+    return (
+        <li className={style.item}>
+            <Image src={"/" + event.image} alt={event.title} width={360} height={300} />
+            <div className={style.content}>
+                <div className={style.summary}>
+                    <h2>{event.title}</h2>
+                    <div className={style.date}>
+                        <time>{formattedDate}</time>
+                    </div>
+                    <div className={style.address}>
+                        <time>{formattedAddress}</time>
+                    </div>
+                </div>
+                <div className={style.actions}>
+                    <Link href={`/events/${event.id}`}>Explore</Link>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default EventCard
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,14 @@
+import EventList from '@/components/Events/EventList'
+import { getFeaturedEvents } from '@/dummy-data'
 import React from 'react'
 
 const HomePage = () => {
+    console.log(getFeaturedEvents())
+
     return (
-        <div>HomePage</div>
+        <div>
+            <EventList events={getFeaturedEvents()} />
+        </div>
     )
 }
 
