c027d6fb31bdea6c4dfc4582a9b0e8f48834f2bb
amar kumar <amarkumar.sharma.124@gmail.com>
2024-01-16 02:52:32 +0530

    added event details

--- components/Events/EventCard.jsx
+++ components/Events/EventCard.jsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 import React from 'react'
 import style from './eventCard.module.css'
 import Button from '../UI/Button.jsx'
+import DateIcon from '../Icons/date-icon'
+import AddressIcon from '../Icons/address-icon'
+import ArrowRightIcon from '../Icons/arrow-right-icon'
 
 const EventCard = ({ event }) => {
     const formattedDate = new Date(event.date).toLocaleDateString('en-US', { day: '2-digit', month: "short", year: "numeric" })
@@ -14,14 +17,16 @@ const EventCard = ({ event }) => {
                 <div className={style.summary}>
                     <h2>{event.title}</h2>
                     <div className={style.date}>
+                        <DateIcon />
                         <time>{formattedDate}</time>
                     </div>
                     <div className={style.address}>
+                        <AddressIcon />
                         <time>{formattedAddress}</time>
                     </div>
                 </div>
                 <div className={style.actions}>
-                    <Button link={`/events/${event.id}`} >Explore</Button>
+                    <Button link={`/events/${event.id}`} ><span>Explore <span className={style.icon}><ArrowRightIcon /></span> </span></Button>
                 </div>
             </div>
         </li>
--- pages/events/[eventID].jsx
+++ pages/events/[eventID].jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { getEventById } from '@/dummy-data'
+import { useRouter } from 'next/router'
+import EventSummary from '@/components/EventsDetail/event-summary'
+import EventLogistics from '@/components/EventsDetail/event-logistics'
+import EventContent from '@/components/EventsDetail/event-content'
 
 const EventDetailPage = () => {
+    const route = useRouter()
+    const eventID = route.query.eventID
+    const event = getEventById(eventID)
+    if (!event) return <p>No event found</p>
     return (
-        <div>EventDetailPage</div>
+        <Fragment >
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent><p>{event.description}</p></EventContent>
+        </Fragment>
     )
 }
 
